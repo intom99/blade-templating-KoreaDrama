9fc6e6bebde0eafd1510ce9462e6cdfc6bd2afaf
intom <iin.arahmah99@gmail.com>
2021-06-13 11:19:01 +0700

    update file

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\templateContoller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/movies', [templateContoller::class, 'movie'])->name('template.movie');
+Route::get('/casts', [templateContoller::class, 'cast'])->name('template.cast');
+Route::get('/directors', [templateContoller::class, 'director'])->name('template.director');
+Route::get('/channels', [templateContoller::class, 'channel'])->name('template.channel');
+Route::get('/', [templateContoller::class, 'index']);
