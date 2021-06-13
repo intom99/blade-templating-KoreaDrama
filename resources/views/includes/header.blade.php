63af254a9796a6fa194e0e5d0b9a2ca75e2bb707
intom <iin.arahmah99@gmail.com>
2021-06-13 11:21:51 +0700

    update view: folder includes

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,61 +1,51 @@
-<section class="h-full w-full border-box transition-all duration-500 linear lg:px-24 md:px-20 px-8 py-8 border-b" style="background-color: #141432">
+<section class="h-full w-full border-box transition-all duration-500 linear lg:px-24 md:px-20 px-8 py-8 bg-white"
+style="  border-bottom: 1px solid #EBEBEB;">
 
-    <div style="font-family: 'Poppins', sans-serif">
-        <div class="mx-auto flex flex-wrap flex-row items-center justify-between">
-            <a class="flex font-medium items-center" href="index.html">
-                <svg
-                width="42"
-                height="42"
-                viewBox="0 0 42 42"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                >
-                <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M3.5 15.75C3.5 8.98451 8.98451 3.5 15.75 3.5H29.75C30.7165 3.5 31.5 4.2835 31.5 5.25C31.5 6.21649 30.7165 7 29.75 7H15.75C10.9175 7 7 10.9175 7 15.75V29.75C7 30.7165 6.2165 31.5 5.25 31.5C4.2835 31.5 3.5 30.7165 3.5 29.75V15.75Z"
-                    fill="white"
-                />
-                <path
-                    d="M10.5 17.5C10.5 13.634 13.634 10.5 17.5 10.5H31.5C35.366 10.5 38.5 13.634 38.5 17.5V31.5C38.5 35.366 35.366 38.5 31.5 38.5H17.5C13.634 38.5 10.5 35.366 10.5 31.5V17.5Z"
-                    fill="white"
-                />
-                </svg>
-            </a>
+<div class="navbar-1-5" style="font-family: 'Poppins', sans-serif;">
+    <div class="container mx-auto flex flex-wrap flex-row items-center justify-between">
+        <a href=""
+        class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center lg:items-center lg:justify-left">
+        <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd"
+            d="M3.50001 15.75C3.50001 8.98451 8.98452 3.5 15.75 3.5H29.75C30.7165 3.5 31.5 4.2835 31.5 5.25C31.5 6.2165 30.7165 7 29.75 7H15.75C10.9175 7 7.00001 10.9175 7.00001 15.75V29.75C7.00001 30.7165 6.21651 31.5 5.25001 31.5C4.28351 31.5 3.50001 30.7165 3.50001 29.75V15.75Z"
+            fill="#27c499" />
+          <path
+            d="M10.5 17.5C10.5 13.634 13.634 10.5 17.5 10.5H31.5C35.366 10.5 38.5 13.634 38.5 17.5V31.5C38.5 35.366 35.366 38.5 31.5 38.5H17.5C13.634 38.5 10.5 35.366 10.5 31.5V17.5Z"
+            fill="#27c499" />
+        </svg>
+      </a>
+      <nav
+        class="lg:flex hidden lg:w-2/5 flex-wrap items-center text-base md:ml-auto lg:space-y-0 space-y-6 lg:space-x-12 space-x-0">
+        <a href="{{url('/')}}" class="nav-link block active">Home</a>
+        <a href="{{url('/movies')}}" class="nav-link block">Movie</a>
+        <a href="{{url('/casts')}}" class="nav-link block">Cast</a>
+        <a href="{{url('/directors')}}" class="nav-link block">Director</a>
+        <a href="{{url('/channels')}}" class="nav-link block">Channel</a>
+      </nav>
+      
+      <label for="menu-toggle" class="cursor-pointer lg:hidden block">
+        <svg class="w-6 h-6" fill="none" stroke="#092A33" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+      </label>
+      <input class="hidden" type="checkbox" id="menu-toggle" />
 
-            <label for="menu-toggle-1-2" class="cursor-pointer lg:hidden block">
-                <svg
-                class="w-6 h-6"
-                fill="none"
-                stroke="#FFFFFF"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-                </svg>
-            </label>
+      <div class="hidden w-full flex-wrap items-center text-base justify-center" id="menu">
+        <nav
+          class="lg:space-x-12 space-x-0 lg:flex items-center justify-between text-base pt-8 lg:pt-0 lg:space-y-0 space-y-6">
+          <a href="{{url('/')}}" class="nav-link block active">Home</a>
+        <a href="{{url('/movies')}}" class="nav-link block">Movie</a>
+        <a href="{{url('/casts')}}" class="nav-link block">Cast</a>
+        <a href="{{url('/directors')}}" class="nav-link block">Director</a>
+        <a href="{{url('/channels')}}" class="nav-link block">Channel</a>
+        </nav>
+      </div>
 
-            <input class="hidden" type="checkbox" id="menu-toggle-1-2" />
-            
-            <div class="hidden lg:flex lg:items-center lg:w-auto w-full lg:ml-auto lg:mr-auto flex-wrap items-center text-base justify-center" id="menu-1-2">
-                <nav class="lg:space-x-12 space-x-0 lg:flex items-center justify-between text-base pt-8 lg:pt-0 lg:space-y-0 space-y-6">
-                    <a class="block cursor-pointer nav-1-2" href="clubs.html">Clubs</a>
-                    <a class="block cursor-pointer nav-1-2" href="players.html">Players</a>
-                    <a class="block cursor-pointer nav-1-2" href="managers.html">Managers</a>
-                    <a class="block cursor-pointer nav-1-2" href="stadiums.html">Stadium</a>
-                </nav>
-            </div>
-            
-            <div class="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu-1-2">
-                <button class="btn-fill-1-2 items-center border-0 py-3 px-8 focus:outline-none rounded-2xl font-medium text-base mt-6 lg:mt-0">
-                    Next Match
-                </button>
-            </div>
-        </div>
+      <div class="lg:w-2/5 lg:inline-flex lg:justify-end ml-0 hidden lg:items-center lg:w-auto w-full" id="menu">
+        <button
+          class="btn-fill text-white items-center border-0 py-3 px-8 focus:outline-none rounded-2xl font-medium text-base mt-6 lg:mt-0">Streaming
+        </button>
+      </div>
     </div>
+  </div>
 </section>
--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -2,63 +2,105 @@
 
 {{-- header style --}}
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+    .navbar-1-5 .btn-fill {
+      background-color: #27c499;
+      transition: 0.3s;
+    }
+
+    .navbar-1-5 .btn-fill:hover {
+      background-color: #27c499;
+      transition: 0.3s;
+    }
+
+    .navbar-1-5 nav a.nav-link:hover,
+    .navbar-1-5 nav a.nav-link.active {
+      font-weight: 500;
+      transition: 0.3s;
+    }
+
+    .navbar-1-5 nav a.nav-link {
+      color: #1B1B1B;
+      transition: 0.3s;
+    }
+
+    .navbar-1-5 #menu-toggle:checked+#menu,
+    .navbar-1-5 #menu-toggle:checked+#menu+#menu {
+      display: block;
+    }
+  </style>
+
+  {{--  --}}
+  <style>
     @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
-    .btn-fill-1-2 {
-        background-color: #ffdda9;
-        color: #372642;
+    .header-2-2 .btn-outline {
+      border: 1px solid #555b61;
+      color: #555b61;
+      transition: 0.3s;
     }
 
-    .btn-fill-1-2:hover {
-        background-color: #fcd396;
-        color: #372642;
+    .header-2-2 .btn-outline:hover {
+      border: 1px solid #27c499;
+      color: #27c499;
+      transition: 0.3s;
     }
-    
-    nav a.nav-1-2 {
-        color: #ffffff;
+
+    .header-2-2 .btn-outline:hover div path {
+      fill: #27c499;
+      transition: 0.3s;
     }
-    
-    nav a.nav-1-2:hover {
-        color: #ffdda9;
-        font-weight: 500;
+
+    .header-2-2 .btn-fill {
+      border: 1px solid #27c499;
+      background-color: #27c499;
+      transition: 0.3s;
     }
-    
-    nav a.nav-1-2.active {
-        color: #ffdda9;
+
+    .header-2-2 .nav-text {
+      color: #1d1e3c;
+      transition: 0.2s;
     }
-    
-    #menu-toggle-1-2:checked + #menu-1-2,
-    #menu-toggle-1-2:checked + #menu-1-2 + #menu-1-2 {
-        display: block;
+
+    .header-2-2 .title-font {
+      color: #272e35;
+      line-height: 1.2
     }
-</style>
+
+    .header-2-2 .navigation a:hover,
+    .header-2-2 .active::after {
+      font-weight: 600;
+      transition: 0.3s;
+    }
+
+    .header-2-2 .small-text {
+      color: #27c499;
+    }
+
+    .header-2-2 .btn-try {
+      background-color: #27c499;
+      transition: 0.3s;
+    }
+  </style>
 
 {{-- footer style --}}
 <style>
-    @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
-    
-    .list-footer-2-3 li a {
-        color: #707092;
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    .footer-2-2 .list-footer li a{
+        color: #C7C7C7;
     }
-    
-    .list-footer-2-3 li a:hover {
-        color: #ffffff;
-        cursor: pointer;
+    .footer-2-2 .list-footer li a:hover{
+        color: #555252;
     }
-
-    .border-color-footer-2-3 {
-        color: #707092;
+    .footer-2-2 .border-color{
+        color: #C7C7C7;
     }
-
-    .footer-link-footer-2-3:hover {
-        color: #ffffff;
-        cursor: pointer;
+    .footer-2-2 .footer-link:hover{
+        color: #555252;
     }
-
-    .social-media-c-footer-2-3:hover circle,
-    
-    .social-media-p-footer-2-3:hover path {
-        fill: #ffffff;
-        cursor: pointer;
+    .footer-2-2 .social-media-c:hover circle,
+    .footer-2-2 .social-media-p:hover path{
+        fill: #555252;
     }
-</style>
+  </style>
