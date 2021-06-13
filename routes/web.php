8c351d94a975baa021a3ace245d32a2052754eaa
intom <iin.arahmah99@gmail.com>
2021-06-14 02:46:00 +0700

    update file

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ Route::get('/movies', [templateContoller::class, 'movie'])->name('template.movie
 Route::get('/casts', [templateContoller::class, 'cast'])->name('template.cast');
 Route::get('/directors', [templateContoller::class, 'director'])->name('template.director');
 Route::get('/channels', [templateContoller::class, 'channel'])->name('template.channel');
-Route::get('/', [templateContoller::class, 'index']);
+Route::get('/', [templateContoller::class, 'index'])->name('index');
